6233cae74e89ba789f0428e3ec8dc71849ae42f3
JosuaCarl <josua.carl@student.uni-tuebingen.de>
2024-12-09 16:46:48 +0100

    Introduced testing general classes

---
+++ tests/test_01_general.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""
+Testing the conversion functions.
+"""
+from tests.common import *
+from source.steps.general import *
+
+platform = get_platform()
+filepath = helpers.get_internal_filepath(__file__)
+out_path, test_path, example_path = contruct_common_paths( filepath )
+
+
+
+def test_step_configuration():
+    pass
+
+def test_pipe_step():
+    pass
