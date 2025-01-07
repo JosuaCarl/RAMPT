84b9ea2b10a76bd7769d127df8e017e265f25bdf
JosuaCarl <josi.carl@gmx.de>
2025-01-07 10:12:45 +0100

    Further adoption of uv

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Use ruff for formatting
-poetry --directory dependencies/python run ruff --config dependencies/python/pyproject.toml format 
+uv run ruff format
 
 # Use ruff for linting
-poetry --directory dependencies/python run ruff --config dependencies/python/pyproject.toml check --fix
+uv run ruff check --fix
