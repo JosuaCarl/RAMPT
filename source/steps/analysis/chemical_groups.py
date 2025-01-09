edaf43fa4a790573cfb11521348d6f71e9c6c1e1
JosuaCarl <josi.carl@gmx.de>
2025-01-09 14:35:46 +0100

    Cleaned notebook scripts

--- source/steps/analysis/chemical_groups.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-"""
-Analyze the statistical significance of the population of chemical groups.
-"""
-
-# Imports
--- source/steps/analysis/pathways.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-Analyze the statistical significance of the population of biological pathways.
-"""
-
-# Imports
