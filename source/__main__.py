f0d82dccc400fdcbac53ba78296d76cb1e9e1841
JosuaCarl <josi.carl@gmx.de>
2025-01-14 13:36:58 +0100

    Added icons, Changelog; Started Release Deploy; pyoxidizer experment; CX-free build try

--- source/__main__.py
+++ source/__main__.py
@@ -4,10 +4,10 @@
 from source.gui.main import *
 
 if __name__ == "__main__":
-    gui = Gui(pages=pages, css_file="main.css")
-    orchestrator = tp.Orchestrator()
+	gui = Gui(pages=pages, css_file="main.css")
+	orchestrator = tp.Orchestrator()
 
-    orchestrator.run()
-    gui.run(
-        title="mine2sirius", port=5001, stylekit=stylekit, async_mode="threading", run_browser=False
-    )
+	orchestrator.run()
+	gui.run(
+		title="mine2sirius", port=5001, stylekit=stylekit, async_mode="threading", run_browser=False
+	)
