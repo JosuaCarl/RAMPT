c1a2f4bdc7a75b0e67eb6759e18eeaa252ff16d5
JosuaCarl <josi.carl@gmx.de>
2025-01-27 16:19:54 +0100

    Re-testing

--- rampt/gui/pages/feature_finding/feature_finding.py
+++ rampt/gui/pages/feature_finding/feature_finding.py
@@ -10,9 +10,6 @@ feature_finding_params = MZmine_Runner()
 
 mzmine_default_folder = os.path.join(Path.home(), ".mzmine", "users")
 
-def read_out_batch_file(batch_path: StrPath):
-    
-
 
 def create_feature_finding():
     with tgb.part(render="{'feat' in entrypoint.lower()}"):
