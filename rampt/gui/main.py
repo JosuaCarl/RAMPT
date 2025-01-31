bea62dd24ed129ca171600ac37f83f7cefa9a03c
JosuaCarl <josi.carl@gmx.de>
2025-01-31 13:28:57 +0100

    Leaving debug mode

--- rampt/gui/main.py
+++ rampt/gui/main.py
@@ -20,15 +20,14 @@ stylekit = {"color_paper_light": "#EFE6F1", "color_background_light": "#EBE5EC"}
 
 def main():
     gui = Gui(pages=pages_dict, css_file="main.css")
-
+    
     orchestrator = tp.Orchestrator()
 
     orchestrator.run(force_restart=True)
     gui.run(
-        title="rampt",
+        title="RAMPT",
         favicon=os.path.join("..", "rampt.ico"),
         port=5001,
         stylekit=stylekit,
         run_browser=True,
-        debug=True,
     )
